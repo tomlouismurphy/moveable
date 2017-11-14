43b9e262ae17f86ead9d2e652bfe0ae3ac053876
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-11-14 14:27:33 -0600

    markers added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import GoogleMap from 'google-map-react';
 import {SelectedEntry} from './SelectedEntry/SelectedEntry.js';
+import {Locations} from './Locations/Locations.js'
 
 class App extends Component {
 	constructor() {
@@ -9,7 +10,19 @@ class App extends Component {
 		this.state = {
 			center: {lat: 41.8781, lng: -87.6298},
 			zoom: 10,
-			entrySelected: true
+			entrySelected: true,
+			locations: [
+				{
+					lat: 41.9484,
+					lng: -87.6553,
+					text: 'Wrigley Field'
+				},
+				{
+					lat: 41.8299,
+					lng: -87.6338,
+					text: 'Guaranteed Rate Field'
+				}
+			]
 		}
 	}
 	selectMemory = (e) => {
@@ -17,13 +30,22 @@ class App extends Component {
 		state.entrySelected = !state.entrySelected;
 		this.setState(state);
 	}
+	renderMarkers(map, maps) {
+		for (let i = 0; i < this.state.locations.length; i++){
+			let marker = new maps.Marker({
+				position: {lat: this.state.locations[i].lat, lng: this.state.locations[i].lng},
+				map,
+				title: this.state.locations[i].text
+			});
+		}
+	}
 	render() {
 		const style = {
-			width: '100%',
-			height: '100%'
+			width: '400px',
+			height: '400px'
 		}
 		return(
-			<div style={{height: "100%", width: "100%", display: "-webkit=flex"}}>
+			<div style={{height: "400px", width: "400px"}}>
 				<GoogleMap
 				style={style}
 				bootstrapURLKeys={{
@@ -32,6 +54,7 @@ class App extends Component {
 				}}
 				defaultCenter={this.state.center}
 				defaultZoom={this.state.zoom}
+				onGoogleApiLoaded={({map, maps}) => this.renderMarkers(map, maps)}
 				>
 					<div id="container">
 						<div id="main-title">
@@ -39,6 +62,7 @@ class App extends Component {
 							<p onClick={this.selectMemory}>TEST</p>
 						</div>
 						{this.state.entrySelected ? <SelectedEntry/> : ''}
+						<Locations lat={41.9484} lng={-87.6553} text={'A'}/>
 					</div>	
 				</GoogleMap>
 			</div>
